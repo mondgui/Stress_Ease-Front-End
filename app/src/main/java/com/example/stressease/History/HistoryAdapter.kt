86147b1147875c19f45f89007aaf7e2912e5d6fa
Gayatridamle <gayatridamle2@gmail.com>
2025-10-08 05:02:01 +0530

    History Package

--- app/src/main/java/com/example/stressease/History/HistoryAdapter.kt
+++ app/src/main/java/com/example/stressease/History/HistoryAdapter.kt
@@ -1,10 +1,11 @@
-package com.example.stressease
+package com.example.stressease.History
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.stressease.R
 
 data class ChatHistoryItem(
     val userMessage: String,
