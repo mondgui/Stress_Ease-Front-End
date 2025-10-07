30106d8bb067d26d83fb22621fa1326939589a21
Gayatridamle <gayatridamle2@gmail.com>
2025-10-08 05:00:33 +0530

    Package API

--- app/src/main/java/com/example/stressease/Api/ApiService.kt
+++ app/src/main/java/com/example/stressease/Api/ApiService.kt
@@ -1,12 +1,13 @@
-package com.example.stressease
+package com.example.stressease.Api
 
+import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 data class ChatRequest(val message: String,val session_id:String?=null)
 data class AiResponse(
@@ -14,7 +15,6 @@ data class AiResponse(
     val timestamp: String,
     val role: String
 )
-
 data class ChatResponse(val reply: String, val emotion: String,val ai_response: AiResponse?,val session_id:String?)
 
 data class CrisisContact(
@@ -28,20 +28,25 @@ data class CrisisContact(
     val country: String?,
     val priority: Int?
 )
+data class CrisisData(@SerializedName("crisis_hotlines") val crisisHotlines: List<CrisisContact>?)
 
 data class CrisisResponse(val success: Boolean,
                           val message: String?,
-                          val contacts: List<CrisisContact>?)
+                          val data: CrisisData?)
 
 interface ApiService {
     @POST("api/chat/message") // for real time chat
-    suspend fun sendMessage(@Header("Authorization") authHeader: String, @Body request:ChatRequest): Response<ChatResponse>
+    suspend fun sendMessage(
+        @Header("Authorization") authHeader: String,
+        @Body request: ChatRequest
+    ): Response<ChatResponse>
 
     @POST("mood/log") //for sentiment analysis /text extraction and generation
     fun logMood(@Body payload: Map<String, String>): Call<Map<String, String>>
 
-    @GET("api/crisis-contacts")
+    @GET("api/chat/crisis-resources")
     suspend fun getCrisisContacts(
-        @Header("Authorization") authHeader: String): Response<CrisisResponse>
+        @Header("Authorization") authHeader: String,
+        @Query("country") country: String
+    ): Response<CrisisResponse>
 }
-
